637ea50cec0377a47852af8c198709b823bd627f
zaizasalsa <zasalsa18@gmail.com>
2021-06-27 18:04:39 +0700

    move skala mmi to home

--- assets/js/data-korban.js
+++ assets/js/data-korban.js
@@ -19,25 +19,28 @@ function readTextFile(file) {
 
 const tableDataKorban = document.querySelector(".tabel-data-korban");
 
-readTextFile("assets/doc/gempa_csv.csv").then((res) => {
-  const dataList = res.split("\n");
-  let th = "";
-  const trHead = document.createElement("tr");
-  for (const data of dataList[0].split(",")) {
-    th += `<th>${data}</th>`;
-  }
-  trHead.innerHTML = th;
-  tableDataKorban.append(trHead);
-  for (let i = 1; i < dataList.length; i++) {
-    const dataItemList = dataList[i].split(",");
-    if (dataItemList.length < 5) break;
-    console.log(dataItemList);
-    const trNode = document.createElement("tr");
-    let td = "";
-    for (const item of dataItemList) {
-      td += `<td>${item}</td>`;
+function fillTable() {
+  readTextFile("assets/doc/gempa_csv.csv").then((res) => {
+    const dataList = res.split("\n");
+    let th = "";
+    const trHead = document.createElement("tr");
+    for (const data of dataList[0].split(",")) {
+      th += `<th>${data}</th>`;
     }
-    trNode.innerHTML = td;
-    tableDataKorban.append(trNode);
-  }
-});
+    trHead.innerHTML = th;
+    tableDataKorban.append(trHead);
+    for (let i = 1; i < dataList.length; i++) {
+      const dataItemList = dataList[i].split(",");
+      if (dataItemList.length < 5) break;
+      console.log(dataItemList);
+      const trNode = document.createElement("tr");
+      let td = "";
+      for (const item of dataItemList) {
+        td += `<td>${item}</td>`;
+      }
+      trNode.innerHTML = td;
+      tableDataKorban.append(trNode);
+    }
+  });
+}
+fillTable();
